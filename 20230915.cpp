ec5d3a8311c7a658083fc14f016d6c668b253f39
Lfw08 <liufw666@outlook.com>
2023-09-15 19:00:29 +0800

    update

--- 20230915.cpp
+++ 20230915.cpp
@@ -1,8 +1,16 @@
 #include <iostream>
-#include <stdio.h>
 using namespace std;
 int main(){
-    
+    int a;
+    cin>>a;
+    cout<<endl;
+
+    if (a%2!=0){
+        cout<<"It's an odd number"<<endl;
+    }
+    else{
+        cout<<"It's an even number"<<endl;
+    }
 
     return 0;
 }
