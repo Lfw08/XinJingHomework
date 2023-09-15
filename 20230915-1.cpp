7530f342cc855801ce1b05ac04ec373385efc647
Lfw08 <liufw666@outlook.com>
2023-09-15 22:27:13 +0800

    update

---
+++ 20230915-1.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+#include <cstdio>
+using namespace std;
+int main(){
+    double weight,hei,bmi;
+    cin>>weight;
+    cin>>hei;
+    bmi = weight/hei;
+    bmi = bmi/hei;
+    printf("%.2f",bmi)
+    return 0;
+}
