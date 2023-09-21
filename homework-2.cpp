ad2bc297de94018a8f111d9b3fec958b99f27f8d
Lfw08 <liufw666@outlook.com>
2023-09-21 18:53:01 +0800

    update

--- homework-2.cpp
+++ homework-2.cpp
@@ -1,8 +1,16 @@
 #include <iostream>
+#include <cstdio>
 using namespace std;
 int main(){
-    int a,b;
-    cin>>a>>b;
-    cout<<b<<" "<<a;
+    int a,b,c,d;
+    int tmp1,tmp2;
+    cin>>a>>b>>c>>d;
+    tmp1 = c - a;
+    tmp2 = d - b;
+    if(tmp2<0){
+        tmp1--;
+        tmp2 = 60 + tmp2;
+    }
+    cout<<tmp1<<" "<<tmp2;
     return 0;
 }
