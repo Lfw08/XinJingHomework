111af2905b42767314ad836453ddef821e7ff545
Lfw08 <liufw666@outlook.com>
2023-09-22 19:32:13 +0800

    update

--- 20230922.cpp
+++ 20230922.cpp
@@ -1,2 +1,14 @@
 #include <iostream>
-#include <cstdio>
+using namespace std;
+int main(){
+    int i;
+    while(i<5){
+        cout<<"Hello"<<endl;
+        i++;
+    }
+    i=0;
+    while(i<5){
+        cout<<"bye"<<endl;
+    }
+    return 0;
+}
