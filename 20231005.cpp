cae4a1330079e2b31716cb665cd35efaa92089f2
Lfw08 <liufw666@outlook.com>
2023-10-05 20:11:20 +0800

    update

--- 20231005.cpp
+++ 20231005.cpp
@@ -2,7 +2,16 @@
 #include <cmath>
 using namespace std;
 int main(){
+    int cnt = 0;
     char a;
-    
+    cin>>a;
+    while(a!='.'){
+        if(a<='z' && a >= 'a'){
+            cnt++;
+            
+        }
+        cin>>a;
+    }
+    cout<<cnt;
     return 0;
 }
