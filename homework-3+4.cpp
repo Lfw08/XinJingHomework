c6c734d623f01a81479ae0f5537293dd2be2f7b7
Lfw08 <liufw666@outlook.com>
2023-10-07 21:29:28 +0800

    update

--- homework-3+4.cpp
+++ homework-3+4.cpp
@@ -1,34 +1,8 @@
-#include <iostream>
-#include <cmath>
-using namespace std;
-int main() {
-    int a=0,b=0,c=0,d=0;
-    long int tmp1,tmp2;
-        
-    while (a<10){
-        tmp1 = 1000*a + 100 *b + 10 *c +d;
-        tmp2 = 10 * a + b + 10 * c + d;
-        if(tmp2 *tmp2 ==tmp1){
-            cout<<a<<b<<c<<d<<endl;
-        }
-        else{}
-        d++;
-        if(d==10){
-            c++;
-            d = 0;
-        }
-        else{}
-        if(c==10){
-            b++;
-            c = 0;
-        }
-        else{}
-        if(b==10){
-            a++;
-            b = 0;
-        }
-        else{}
-    }
-    
-    return 0;
-}
+     #include <iostream>
+     using namespace std;
+     int main(){
+         int i,k;
+         cin>>i>>k;
+         cout<<i+(i-1)/(k-1);
+         return 0;
+     } 
