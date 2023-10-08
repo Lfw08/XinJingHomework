04e9ad2150a88c4a6eb0db4b7509a082d31c6571
Lfw08 <liufw666@outlook.com>
2023-10-08 21:43:12 +0800

    update

--- homework-3+4.cpp
+++ homework-3+4.cpp
@@ -1,8 +1,21 @@
-     #include <iostream>
-     using namespace std;
-     int main(){
-         int i,k;
-         cin>>i>>k;
-         cout<<i+(i-1)/(k-1);
-         return 0;
-     } 
+#include <iostream>
+using namespace std;
+int main(){
+    double n,m,i,num = 0,a;
+    cin>>m>>n;
+    if(m>n){
+        a = m;
+    }
+    else{
+        a = n;
+    }
+    for ( i=1 ; i<=a ; ++i) {
+        if((int)n%(int)i ==0){
+            if((int)m%(int)i==0){
+                num = i;
+            }
+        }
+    }
+    cout<<num<<endl;
+    return 0;
+}
