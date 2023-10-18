4956005f2c61fd2a176c1d3d463640589c38dc00
Lfw08 <liufw666@outlook.com>
2023-10-18 08:56:37 +0000

    update

--- homework5.cpp
+++ homework5.cpp
@@ -1,20 +1,16 @@
 #include <iostream>
 using namespace std;
 int main(){
-    int n,i;
-    bool a = 1;
-    for (i = 2,i<n,i++){
-        if (n%i=0){
-            a = 0;
-        }
-        if(a=0){
-            cout<<"F"<<endl;
-            break;
-        }
-        
-    }
-    if(a = 1){
-        cout<<"T"<<endl;
-    }
+    int m;
+	cin>>m;
+	for(int i = 0;i<m;i++){
+		for(int g=1;g<m;g++){
+				cout<<" ";
+			}
+		for(int g=0;g<(m-i);g++){
+				cout<<"*";
+			}
+		cout<<endl;
+	} 
     return 0;
 }
