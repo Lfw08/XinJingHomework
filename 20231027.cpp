cb1ef4b422558c0a0770501633f609b0769374e3
Lfw08 <liufw666@outlook.com>
2023-11-01 00:20:24 +0800

    update

---
+++ 20231027.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+using namespace std;
+int main(){
+    int n,b;
+    cin>>n;
+    double a[n+2];
+    for(int i=1;i<=n;i++){
+        cin>>a[i];
+    }
+    cin>>b;
+    for(int i=1;i<=n;i++){
+        
+    }
+    return 0;
+}
