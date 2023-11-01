45ff2f94d174e38ab00944d4855e218f09297265
Lfw08 <liufw666@outlook.com>
2023-11-01 22:34:34 +0800

    update

--- homewwork8.cpp
+++ homewwork8.cpp
@@ -1,29 +1,22 @@
-#include <bits/stdc++.h>
-using namespace std;
-int t;
-int main(){
-    int a[5] = {10, 20, 30, 40, 50};
-    for(int i=0;i<4;i++){
-        cin>>a[i];
+
+#include <iostream>
+
+int main() {
+    int a[4];
+    for (int i = 0; i < 4; i++) {
+        std::cin >> a[i];
     }
-    int min=INT_MAX;
-    int min_pos=0;
-    for(int j=0;j<4;j++){
-        //find the minimum number in a[j] to a[4]
-        if (min > a[j]){
-            min = a[j];
-            min_pos = j+1;
+    for (int i = 0; i < 3; i++) {
+        int min_index = i;
+        for (int j = i + 1; j < 4; j++) {
+            if (a[j] < a[min_index]) {
+                min_index = j;
             }
-        //exchange positions
-        t=a[min_pos];
-        a[min_pos]=a[j];
-        a[j]=t;
-        
+        }
+        std::swap(a[i], a[min_index]);
     }
-    //output the new a
-    cout<<a[3]<<" ";
-    cout<<a[0]<<" ";
-    cout<<a[2]<<" ";
-    cout<<a[1]<<" ";
+    for (int i = 0; i < 4; i++) {
+        std::cout << a[i] << " ";
+    }
     return 0;
 }
