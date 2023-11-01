095b13240693830cd147b872a10ff32f74506c58
FangWen Liu <liufw666@outlook.com>
2023-11-01 08:13:12 +0000

    update

--- homewwork8.cpp
+++ homewwork8.cpp
@@ -2,7 +2,7 @@
 using namespace std;
 
 int main(){
-    int a[5] = {10, 20, 30, 40, 50}; // Correctly initialize the array
+    int a[5] = {10, 20, 30, 40, 50};
     for(int i=0;i<4;i++){
         cin>>a[i];
     }
