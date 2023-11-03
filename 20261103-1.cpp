1710f2ee84acb52b72eebb04309201ab1f100091
Lfw08 <liufw666@outlook.com>
2023-11-03 19:34:41 +0800

    update

--- 20261103-1.cpp
+++ 20261103-1.cpp
@@ -3,29 +3,53 @@
 #include<ctime>
 using namespace std;
 const int N=10;
-int main(){
-	srand((unsigned)time(NULL));
-	int a[N];
-	int i,j,x,l,r,m;
-	for(i=0;i<N;i++){
-		a[i]=rand()%1000;
-	}  
-	for(i=1;i<N;i++){
-		x=a[i];r=i-1;l=0;
-		while(l<=r){
-			m=(l+r)/2;
-		 	if (x<a[m]){
-				r=m-1;
-			}else{
-				l=m+1;
-			}
-		}
-		for (j=i-1;j>=l;j--){
-			a[j+1]=a[j];//后移
-		}
-		a[l]=x;
-	}
-	for(i=0;i<N;i++)
-	{cout<<a[i]<<endl;}   
-	return 0; 
+#include <iostream>
+#include <cstdlib>
+#include <ctime>
+
+const int N = 10; // Assuming N is a constant
+
+int main() {
+    // Seed the random number generator with the current time
+    srand((unsigned)time(NULL));
+
+    // Create an array of size N
+    int a[N];
+
+    // Initialize the array with random values between 0 and 999
+    for (int i = 0; i < N; i++) {
+        a[i] = rand() % 1000;
+    }
+
+    // Perform the insertion sort algorithm on the array
+    for (int i = 1; i < N; i++) {
+        int x = a[i]; // Store the current element
+        int l = 0; // Left pointer
+        int r = i - 1; // Right pointer
+
+        // Find the correct position for the current element in the sorted subarray
+        while (l <= r) {
+            int m = (l + r) / 2; // Middle index
+            if (x < a[m]) {
+                r = m - 1; // Move the right pointer
+            } else {
+                l = m + 1; // Move the left pointer
+            }
+        }
+
+        // Shift the elements to the right of the correct position
+        for (int j = i - 1; j >= l; j--) {
+            a[j + 1] = a[j];
+        }
+
+        // Insert the current element at the correct position
+        a[l] = x;
+    }
+
+    // Print the sorted array
+    for (int i = 0; i < N; i++) {
+        std::cout << a[i] << std::endl;
+    }
+
+    return 0;
 }
