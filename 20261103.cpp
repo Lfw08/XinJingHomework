b7ed5e7b664b8c780475f4531926b4f74e9ee145
Lfw08 <liufw666@outlook.com>
2023-11-03 18:28:29 +0800

    update

--- 20261103.cpp
+++ 20261103.cpp
@@ -3,8 +3,8 @@ using namespace std;
 char a[256];
 int main(){
     int cnt=0;
-    cin>>a;
-    for(int i=0;i<255;i++){
+    cin.getline(a,256);
+    for(int i=0;i<256;i++){
         if(a[i]>='0'&& a[i]<='9'){
             cnt++;
         }
