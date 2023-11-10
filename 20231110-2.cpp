5944346f1cd6ec45f42f6becd924a502b661cb54
Lfw08 <liufw666@outlook.com>
2023-11-10 20:43:09 +0800

    update

--- 20231110-2.cpp
+++ 20231110-2.cpp
@@ -1,35 +1,31 @@
 #include <bits/stdc++.h>
-char a[1086],b[1086],c[10086],s1[1086],s2[1086];
+char s1[1086],s2[1086];
+int a[1086],b[1086],c[10086];
 using namespace std;
 int main(){
     int len1,len2;
     cin>>s1>>s2;
     len1=strlen(s1);
     for(int i=0;i<len1;i++){
-        a[i]=s1[len1-i-1];
+        a[i]=s1[len1-i-1-'0'];
     }
     len2=strlen(s2);
     for(int i=0;i<len2;i++){
-        b[i]=s1[len2-i-1];
+        b[i]=s1[len2-i-1-'0'];
     }
     for(int i=0;i<max(len1,len2);i++){
         c[i]=a[i]+ b[i];
     }
-    for(int i=0;i<strlen(c);i++){
-        c[i+1]+=c[i]/10;
-        c[i]+=c[i]%10;
+    for(int i=0;i<max(len1,len2);i++){
+        c[i+1]=(a[i]+b[i]+c[i])/10;
+        c[i]=(a[i]+b[i]+c[i])%10;
     }
-    bool d=true;
-    int i=strlen(c);
-    while (d){
-        if(c[i]==0){
-            i--;
-            continue;
-        }else{d=false;}
-    }
-    cout<<i<<endl;
-    for(i+=0;i>=0;i--){
-        cout<<c[i];
+    //output c in reversed order and ignore '0's at the beginning
+    for(int i=max(len1,len2)-1; i>=0; i--){
+        if(c[i] != 0){
+            cout << c[i];
+        }
     }
     return 0;
-}
+    }
+    
