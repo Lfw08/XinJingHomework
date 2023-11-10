5f0b3e9f6bd0cce317767765f9778374184a3504
Lfw08 <liufw666@outlook.com>
2023-11-10 18:57:38 +0800

    update

--- 20231110-2.cpp
+++ 20231110-2.cpp
@@ -1,5 +1,20 @@
 #include <bits/stdc++.h>
 using namespace std;
+int cnt[11];
+int a[7];
 int main(){
     
+    //input
+    for(int i=1;i<=7;i++){
+        cin>>a[i];
+    }
+    
+    for(int i=1;i<=10;i++){
+        cnt[a[i]]++;
+    }
+    for(int i=1;i<=10;i++){
+        for(int j=1;j<=cnt[i];j++){
+            cout<<i<<" ";
+        }
+    }
 }
