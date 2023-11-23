f1acc487b0ffdbdfb79dcdba3fbc0d7c3f4d53b8
Lfw08 <liufw666@outlook.com>
2023-11-23 22:01:05 +0800

    update

--- homework-10-2.cpp
+++ homework-10-2.cpp
@@ -6,7 +6,6 @@ int main() {
     string isbn;
     cin >> isbn;
 
-    // Remove the hyphens from the input ISBN number
     string digits;
     for (char c : isbn) {
         if (c != '-') {
@@ -14,28 +13,23 @@ int main() {
         }
     }
 
-    // Calculate the checksum
-    int checksum = 0;
+    int sum1 = 0;
     for (int i = 0; i < 9; i++) {
-        checksum += (i + 1) * (digits[i] - '0');
+        sum1 += (i + 1) * (digits[i] - '0');
     }
-    checksum %= 11;
+    sum1 %= 11;
 
-    // Determine the correct checksum digit
-    char correctChecksum;
-    if (checksum == 10) {
-        correctChecksum = 'X';
+    char corr_sum;
+    if (sum1 == 10) {
+        corr_sum = 'X';
     } else {
-        correctChecksum = checksum + '0';
+        corr_sum = sum1 + '0';
     }
-
-    // Compare the calculated checksum with the last digit of the input ISBN number
-    if (correctChecksum == digits[9]) {
+    if (corr_sum == digits[9]) {
         cout << "Right" << endl;
     } else {
-        // Replace the last digit with the correct checksum
-        digits[9] = correctChecksum;
-        // Output the corrected ISBN number
+        digits[9] = corr_sum;
+
         cout << digits.substr(0, 1) << "-" << digits.substr(1, 3) << "-"
              << digits.substr(4, 5) << "-" << digits.substr(9, 1) << endl;
     }
