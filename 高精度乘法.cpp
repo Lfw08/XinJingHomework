db6cf944e24fac13c8a64d437141feabccbbe657
Lfw08 <liufw666@outlook.com>
2023-11-24 20:08:44 +0800

    update

---
+++ 高精度乘法.cpp
@@ -0,0 +1,11 @@
+#include <bits/stdc++.h>
+using namespace std;
+int main(){
+    char s1[10086],s2[10086];
+    char a[10086],b[10086],c[200000];
+    cin>>s1;
+    cin>>s2;
+    
+
+    return 0;
+}
