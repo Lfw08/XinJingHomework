3c2f26d374bd45da41d84251935d4d2f9946caa5
Lfw08 <liufw666@outlook.com>
2023-12-01 11:47:12 +0000

    update

--- 20231202.cpp
+++ 20231202.cpp
@@ -3,15 +3,18 @@ using namespace std;
 double d;
 void f(double x,double y){
     double z=x+y;
+    cout<<z<<endl;
     cout<<z/2;
 }
 double g(double x,double y){
     double z=x+y;
+    cout<<z<<endl;
     z/=2;
     return z;
 }
 void h(double x,double y){
     d=x+y;
+    cout<<d<<endl;
     d/=2;
 }
 int main(){
