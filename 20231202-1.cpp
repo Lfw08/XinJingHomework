68d6ffd8a36fe9a55b499a80e48778fe494cba11
Lfw08 <liufw666@outlook.com>
2023-12-01 11:59:33 +0000

    update

---
+++ 20231202-1.cpp
@@ -0,0 +1,22 @@
+//判断素数（函数版）
+// ↑ If this doesn't show, use Visual Studio Code
+#include <bits/stdc++.h>
+bool a(int a){
+    bool b=1;
+    for(int i=2;i<a;i++){
+        if(b%i==0){
+            b=0;
+        }
+    }
+    return b;
+}
+int main(){
+    int x;
+    std::cin>>x;
+    if(a(x)){
+        std::cout<<"Yes";
+    }else{
+        std::cout<<"No";
+    }
+    return 0;
+}
