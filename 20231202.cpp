b6bb804382f4cca4e71ea1b21007b0c05d0a1847
Lfw08 <liufw666@outlook.com>
2023-12-01 11:43:00 +0000

    update

--- 20231202.cpp
+++ 20231202.cpp
@@ -1,12 +1,23 @@
 #include <bits/stdc++.h>
 using namespace std;
-double f(double x,double y){
+double d;
+void f(double x,double y){
     double z=x+y;
-    cout<<z;
+    cout<<z/2;
+}
+double g(double x,double y){
+    double z=x+y;
+    z/=2;
+    return z;
+}
+void h(double x,double y){
+    d=x+y;
+    d/=2;
 }
 int main(){
     double x,y;
     cin>>x>>y;
-    f(x,y);
+    h(x,y);
+    cout<<d;
     return 0;
 }
