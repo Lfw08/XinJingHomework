bed0cf443f1513d09cde1bfeccf1ca6717711035
Lfw08 <liufw666@outlook.com>
2023-12-01 11:17:40 +0000

    update

--- 20231202.cpp
+++ 20231202.cpp
@@ -1,2 +1,12 @@
 #include <bits/stdc++.h>
 using namespace std;
+double f(double x,double y){
+    double z=x+y;
+    return z;
+}
+int main(){
+    double x,y;
+    cin>>x>>y;
+    cout<<f(x,y);
+    return 0;
+}
