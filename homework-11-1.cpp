aba0a7d86a1aea21386eefd76fc853fa15a56264
Lfw08 <liufw666@outlook.com>
2023-12-02 22:40:56 +0800

    update

--- homework-11-1.cpp
+++ homework-11-1.cpp
@@ -1,8 +1,27 @@
 #include <bits/stdc++.h>
-using namespace std;
-int main(){
-    
+char binary[32];
+// Function to convert decimal to binary
+void toBinary(int n) {
+    int i = 0;
 
+    // Convert decimal to binary using division by 2
+    while (n > 0) {
+        binary[i++] = (n % 2) + '0';
+        n /= 2;
+    }
 
+    binary[i] = '\0';
+}
+
+int main() {
+    int n;
+    std::cin >> n;
+    toBinary(n);
+
+    // Print the binary representation
+    int len=strlen(binary);
+    for (int i = len - 1; i >= 0; i--) {
+        std::cout << binary[i];
+    }
     return 0;
 }
