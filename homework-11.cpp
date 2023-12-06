a125a0048fee8a66efc4048b609b911bbe45467b
Lfw08 <liufw666@outlook.com>
2023-12-06 08:45:41 +0000

    update homework-11 and 12

--- homework-11.cpp
+++ homework-11.cpp
@@ -1,9 +1,16 @@
 #include <bits/stdc++.h>
 using namespace std;
+
 struct student{
     int grade;
     string name;
 };
+
+// Define a custom comparator function to sort by grade
+bool compareByGrade(const student &a, const student &b){
+    return a.grade < b.grade;
+}
+
 int main(){
     int n;
     cin>>n;
@@ -11,26 +18,15 @@ int main(){
     for(int i=0;i<n;i++){
         cin>>s[i].name>>s[i].grade;
     }
-    // Sort by grade
 
-    for (int i = 0; i < n-1; i++) {
-        int min_index = i;
-        for (int j = i + 1; j < n; j++) {
-            if (s[j].grade < s[min_index].grade) {
-                min_index = j;
-            }
-        }
-        std::swap(s[i].name, s[min_index].name);
-        std::swap(s[i].grade,s[i+1].grade);
-    }
-
+    // Sort by grade using std::sort
+    std::sort(s, s+n, compareByGrade);
 
     //output the names and grade
     cout<<endl;
-    for(int i=0;i<n;i++){
+    for(int i=n;i>=0;i--){
         cout<<s[i].name<<" "<<s[i].grade<<endl;
     }
-
 
     return 0;
 }
