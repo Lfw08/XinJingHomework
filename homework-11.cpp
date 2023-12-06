4c4b5e648a5b25c46aed6d3e76cee10117b57c9b
Lfw08 <liufw666@outlook.com>
2023-12-06 08:28:25 +0000

    update

--- homework-11.cpp
+++ homework-11.cpp
@@ -11,15 +11,20 @@ int main(){
     for(int i=0;i<n;i++){
         cin>>s[i].name>>s[i].grade;
     }
+    // Sort by grade
+
     for (int i = 0; i < n-1; i++) {
         int min_index = i;
-        for (int j = i + 1; j < 4; j++) {
+        for (int j = i + 1; j < n; j++) {
             if (s[j].grade < s[min_index].grade) {
                 min_index = j;
             }
         }
-        std::swap(s[i], s[min_index]);
+        std::swap(s[i].name, s[min_index].name);
+        std::swap(s[i].grade,s[i+1].grade);
     }
+
+
     //output the names and grade
     cout<<endl;
     for(int i=0;i<n;i++){
