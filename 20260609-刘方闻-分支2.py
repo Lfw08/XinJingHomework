4b993ff0abfb7d8a44f97de76ce32ed9fbe3bec9
Lfw08 <liufw666@outlook.com>
2023-12-22 12:33:24 +0000

    update

--- 20260609-刘方闻-分支2.py
+++ 20260609-刘方闻-分支2.py
@@ -1,17 +1,21 @@
+# -*- coding: utf-8 -*-
+"""
+Spyder Editor
 
+This is a temporary script file.
+"""
 import tkinter as tk
 import tkinter.ttk as ttk
 import time
 root=tk.Tk()
 global label2
-label2=ttk.Label(root,text="炸机，启动！")
+label2=ttk.Label(root,text="启动！")
 
 def func1():
-    label2.grid()
-    time.sleep(5)
+    label2.grid(row=1,column=2)
 
-label1=ttk.Button(root,text="老芦",command=func1)
-label1.grid(row=0,column=0)
+label1=ttk.Button(root,text="王析多，",command=func1)
+label1.grid(row=1,column=1)
 
 frame = tk.Frame(root)
 
