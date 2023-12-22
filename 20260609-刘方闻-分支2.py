c335c4dfd9e5de3c91706da7142796cf3810c60c
Lfw08 <liufw666@outlook.com>
2023-12-22 22:35:54 +0800

    update

--- 20260609-刘方闻-分支2.py
+++ 20260609-刘方闻-分支2.py
@@ -1,17 +1,22 @@
 
 import tkinter as tk
 import tkinter.ttk as ttk
+import tkinter.simpledialog as dlg
 import time
+
 root=tk.Tk()
 global label2
-label2=ttk.Label(root,text="启动！")
+label2=ttk.Label(root,text="   炸机！   ")
 
 def func1():
     label2.grid(row=1,column=2)
+    root.update()
+    time.sleep(1.5)
+    dlg1=dlg.Dialog(root,title="炸机成功！")
 
-label1=ttk.Button(root,text="王析多，",command=func1)
+label1=ttk.Button(root,text="    老芦，   ",command=func1)
 label1.grid(row=1,column=1)
 
-frame = tk.Frame(root)
+frame = tk.Frame(root,height=600,width=800)
 
 root.mainloop()
