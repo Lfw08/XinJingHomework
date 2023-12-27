91bd2fc165a254f4153d9c789177caa3c9a528ac
Lfw08 <liufw666@outlook.com>
2023-12-27 09:02:14 +0000

    update

--- 20231222.cpp
+++ 20231222.cpp
@@ -1,22 +1,15 @@
 #include<bits/stdc++.h>
 using namespace std;
-const int n=3;
 int b[4]={0};
 bool a[4]={true};
-int main(){
-    void f(int k);
-    f(1);
-
-return 0;
-}
 void f(int k){
     
-    if(k>n)
-        for(int i=0;i<n;i++){
+    if(k>3)
+        for(int i=0;i<3;i++){
             cout<<b[i]<<" ";
         }
     else{
-        for(int i=1;i<=n;i++){
+        for(int i=1;i<=3;i++){
             if(a[i]){
                 a[i]=false;
                 b[k]=i;
@@ -25,4 +18,9 @@ void f(int k){
                 b[k]=0;
         }}
     }
+}
+int main(){
+    void f(int k);
+    f(1);
+return 0;
 }
--- homework-13.cpp
+++ homework-13.cpp
@@ -1,37 +1,12 @@
-#include<bits/stdc++.h>
+#include <bits/stdc++.h>
 using namespace std;
-int main()
-{
-	int num;
-	double sum1,s1=0; 
-	int a[1001],b[1001];     
-	cin>>num;
-	for(int i=1;i<=num;i++)    
-	{
-		cin>>a[i];
-		b[i]=i;
-	}
-	for(int i=1;i<=num;i++)    
-	{
-		for(int j=i;j<=num;j++)
-		{
-			if(a[i]>a[j])
-			{
-				swap(a[i],a[j]);
-				swap(b[i],b[j]);
-			}
-		}
-	}
-	for(int i=1;i<=num;i++)
-	{
-		s1+=a[i]*(num-i);
-	}
-	sum1=s1/num;
-	for(int i=1;i<=num;i++)
-	{
-		cout<<b[i]<<" ";
-	}
-	cout<<endl;
-	printf("%.2f\n",sum1);
-	return 0;
+int main(){
+    int n,m,num;
+    cin>>n;
+    cin>>n;
+    
+
+
+
+    return 0;
 }
