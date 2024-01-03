6ba2d7cb62a26acdf56e369c29b554ce9288a2de
Lfw08 <liufw666@outlook.com>
2024-01-03 09:03:37 +0000

    update

--- homework-13.cpp
+++ homework-13.cpp
@@ -1,12 +1,30 @@
 #include <bits/stdc++.h>
 using namespace std;
 int main(){
-    int n,m,num;
+    int n,m,sum=0,ans=0;
     cin>>n;
-    cin>>n;
-    
+    cin>>m;
+    // 输入
+    int a[n];
+    for(int i=0;i<n;i++){
+        cin>>a[i];
+    }
+    //遍历，判断是不是超出最大
+    for(int i=0;i<n;i++)
+    {
+        if(a[i]+sum>m)
+        {
+            sum=0;
+            ans++;
+        }
+        sum=sum+a[i];
+    }
+    if(sum)
+    {
+        ans++;
+    }
+    // 输出
 
-
-
+    cout<<ans;
     return 0;
 }
