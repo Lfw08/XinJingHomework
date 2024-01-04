d75785ba879f33558dc6146a8308fca1edfde4be
Lfw08 <liufw666@outlook.com>
2024-01-04 22:40:49 +0800

    update

--- 排列-乔云浩.cpp
+++ 排列-乔云浩.cpp
@@ -1,10 +1,9 @@
-#include<iostream>
-//与八皇后完全一样 
+#include<bits/stdc++.h>
 using namespace std;
 bool a[9999];
 int b[9999];
 int n;
-void pailie(int k){//排第k个位置 
+void pailie(int k){
 	int i;
 	if(k>n) {
 		for(i=1;i<=n;i++) {
@@ -15,11 +14,11 @@ void pailie(int k){//排第k个位置
 	}
 	for(i=1;i<=n;i++)  {
 		if(a[i]==true) {
-			b[k]=i;//存结果 
-			a[i]=false;//改状态 
-			pailie(k+1);//尝试下一个位置 
-			b[k]=0;//恢复结果 
-			a[i]=true;//恢复状态 
+			b[k]=i;
+			a[i]=false;
+			pailie(k+1);
+			b[k]=0;
+			a[i]=true;
 		}
 	}    
 }
