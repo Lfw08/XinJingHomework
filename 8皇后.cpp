62f7ee7f1caae4fdef8a6e275ba7fcc9423c7937
Lfw08 <liufw666@outlook.com>
2024-01-05 12:01:13 +0000

    update

--- 8皇后.cpp
+++ 8皇后.cpp
@@ -2,17 +2,19 @@
 using namespace std;
 int a[9];//结果
 int b[9]={1};//是否能用
-int c[30];//和一定
-int d[30];//差一定
+int c[30]={1};//和一定
+int d[30]={1};//差一定
 
 void queen(int k){
+    cout<<k;
     //尝试第n行
-    if(k==9){
+    if(k>8){
         //输出结果
         for(int i=1;i<=8;i++){
             cout<<i<<","<<a[i]<<" " ;
             cout<<endl;
         }
+        
     }else{
         for(int i=1;i<=8;i++){//尝试第n列
             if(b[i]==1 && c[k+i+7]==1 && d[k-i+7]==1){
