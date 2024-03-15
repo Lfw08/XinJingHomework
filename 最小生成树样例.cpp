8c7bd2ba9e8b0a6211265518b10dbc5fe6546fd2
lfw08 <liufw666@outlook.com>
2024-03-15 19:53:59 +0800

    update

--- 最小生成树样例.cpp
+++ 最小生成树样例.cpp
@@ -1,36 +1,39 @@
 #include <bits/stdc++.h>
 using namespace std;
 
-int n,m,out;
-vector<int>gh[110];
+int n, m;
+int gh[110][110];
 int vis[110];
+int out = 0;
+int tmpk, tmpj, w;
 
-int main(){
-    int tmpk,tmpj,w;
-	cin>>n;
-	for(int i=1;i<=n;i++){
-		for(int g=1;g<=n;g++){
-			cin>>gh[n][g+1];
-		}
-	}
-	vis[0]=1;
-	for(int j=0;j<n;j++){
-		if(vis[j]==1){//找在树里的点
-		int w=1000;
-		if(vis[j]==1){
-			for(int k=0;k<n;k++){
-			if(vis[k]==0 && gh[j][k]<=w){
-			int tmpj=j;
-			int tmpk=k;
-			w=gh[j][k];
-		}
-		}
-		}
-		 	
-		}
-		vis[tmpk]=1;
-		out += w;
-		cout<<tmpj<<"-----"<<tmpk<<endl;
-	}
-	return 0;
-	}
+int main() {
+    cin >> n;
+    for (int i = 1; i <= n; i++) {
+        for (int g = 1; g <= n; g++) {
+            cin >> gh[i][g];
+        }
+    }
+    vis[0] = 1;
+    for (int num = 0; num < n - 1; num++) {
+        for (int j = 0; j < n; j++) {
+            if (vis[j] == 1) {
+                w = 1000;
+                for (int k = 0; k < n; k++) {
+                    if (vis[k] == 0 && gh[j][k] <= w) {
+                        tmpj = j;
+                        tmpk = k;
+                        w = gh[j][k];
+                        break;
+                    }
+                }
+            }
+        }
+        vis[tmpk] = 1;
+        out += w;
+        cout << tmpj << "-----" << tmpk << endl;
+    }
+    cout<<endl;
+    cout<<out;
+    return 0;
+}
