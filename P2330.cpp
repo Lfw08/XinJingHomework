d2dfd319034818aa7b10f6955d06b879a49f1f01
Lfw08 <liufw666@outlook.com>
2024-03-22 10:38:28 +0000

    Update Prim's algorithm implementation for finding minimum spanning tree
    Changes include:
    
    - Changing variable names from `u`, `s` to `enter`, `pending`
    - Initializing an array `pending` to keep track of minimum distances to unvisited nodes
    - Updating the logic to select the smallest distance and update the `pending` array accordingly

--- P2330.cpp
+++ P2330.cpp
@@ -1,37 +1,40 @@
 #include<bits/stdc++.h>
+//Prim 算法
 using namespace std ;
-int u[2505] ;//是否加入树 
-int s[2505] ;//没加入树的节点 
+int enter[2505] ;
+int pending[2505] ;
 int mp[2505][2505] ; //两条路之间的分值 
 int main()
 {
-	memset(mp , 1 , sizeof(mp)) ;//初始化mp  
-	memset(s , 1 , sizeof(s)) ;//初始化s 
+	//初始化
+	memset(mp , 1 , sizeof(mp)) ;  
+	memset(pending , 1 , sizeof(pending)) ;
 	
 	int n , m , ans = 0 ;
 	cin>>n>>m; 
 	for(int i = 1 ; i <= m ; ++i)
 	{
+		//3个边
 		int u , v , c ;
 		scanf("%d%d%d" , &u , &v , &c) ;
 		if(c < mp[u][v])//选择分值小的 
 			mp[u][v] = mp[v][u] = c ; 
 	}
-	s[1] = 0 ;//设置起点  
+	pending[1] = 0 ;//设置起点  
 	for(int i = 1 ; i <= n ; ++i)
 	{
 		int k = 0 ;
-		for(int j = 1 ; j <= n ; ++j)//寻找还没有进入树但是s最小的路 
-			if(u[j] == 0 && s[j] < s[k])
+		for(int j = 1 ; j <= n ; ++j)//还没有进入树但是s最小
+			if(enter[j] == 0 && pending[j] < pending[k])
 				k = j ;
-		u[k] = 1 ;
+		enter[k] = 1 ;
 		for(int j = 1 ; j <= n ; ++j)
-			if(u[j] == 0 && mp[k][j] < s[j])
-				s[j] = mp[k][j] ;
+			if(enter[j] == 0 && mp[k][j] < pending[j])
+				pending[j] = mp[k][j] ;
 	}
 	for(int i = 1 ; i<= n ; ++i)//寻找分值最大的路 
-		if(s[i] > ans)
-			ans = s[i] ;
-	cout<<n-1<<ans<<" ";
+		if(pending[i] > ans)
+			ans = pending[i] ;
+	cout<<n-1<<" "<<ans;
 	return 0 ;
 }
