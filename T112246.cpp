f7cca6b5002f5dcfa9690b3575e2ff9727fbcca8
FangWen Liu <liufw666@outlook.com>
2024-06-14 11:47:14 +0000

    update

--- T112246.cpp
+++ T112246.cpp
@@ -1,8 +1,9 @@
 #include <bits/stdc++.h>
 using namespace std;
 
-
+int cnt,maxdep;
 char tree[1145];
+int dep[1145];
 
 void buildtree(int x){
     cin>>tree[x];
@@ -13,7 +14,45 @@ void buildtree(int x){
     }
 }
 
+void inOrder(int r){ //中序
+    if(tree[r] == '#'){
+        return;
+    }
+    inOrder(2*r);
+    cout<<tree[r];
+    inOrder(2*r + 1);
+}
+
+void left(int r){
+    if(tree[r] == '#'){
+        return ;
+    }
+    if(tree[2*r] == '#' && tree[2*r + 1] == '#'){
+        cnt++;
+    }
+    left(2*r);
+    left(2*r + 1);
+}
+
+void depth(int r){
+    if(tree[r] == '#'){
+        return;
+    }
+    dep[r]= dep[r/2] + 1;
+    if(maxdep < dep[r]){
+        maxdep = dep[r];
+    }
+    depth(r);
+    depth(2*r);
+}
+
+void postOrder(){
+    
+}
+
+
+
+
 int main(){
     buildtree(1);
-    
 }
