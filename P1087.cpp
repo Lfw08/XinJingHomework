d69fc36b47acf04e0261762cff6464805fe9fa94
FangWen Liu <liufw666@outlook.com>
2024-06-21 11:10:09 +0000

    update

--- P1087.cpp
+++ P1087.cpp
@@ -1,17 +1,38 @@
 #include <bits/stdc++.h>
 using namespace std;
 
-void guibing(int left, int right){
-    if((right-left)==1){
-        return;
-    }//此处为出口
-    int mid = (left + right )/2;
-    guibing(left,mid);
-    guibing(mid,right);
-    int i = left;
-    int zl = left;
-    int zr = mid;
+char tmp[1145];
+int n;
 
-    
+void tree(int left,int right){
+    if(right>left){
+        tree(left,(left+right)/2);
+        tree(((left+right)/2)+1,right);
+    }    
 
+    int i,cnt0 = 0,cnt1 = 0;
+    for(int i = left; i<= right;i++){
+        if(tmp[i]=='0'){
+            cnt0++;
+        }
+        else{
+            cnt1++;
+        }
+    }
+    if(cnt0 && cnt1){
+        cout<<"F";
+    }else if(cnt0){
+        cout<<"B";
+    }
+    else{
+        cout<<"I";
+    }
+}
+
+int main(){
+    cin>>n;
+    cin>>tmp;
+
+    tree(0,(1<<n)-1);
+    return 0;
 }
