7ebd19f859e7046b792ef91a77915cb246b961bb
Lfw08 <liufw666@outlook.com>
2024-07-14 02:03:59 +0000

    20240714-2:

--- P6560.cpp
+++ P6560.cpp
@@ -1,10 +1,31 @@
 #include <bits/stdc++.h>
 using namespace std;
+const int maxn = 1e5+5;
 int n,m,q;
-vector<int> a;
+int a[maxn][maxn];
 
+void bfs(int x,int target){
+    //从x开始
+    queue<int> q;
+    q.push(x);
+    while(!q.empty()){
+        int xtmp = q.front();
+        q.pop();
+        
 
+    }
+}
 
 int main(){
+    cin>>n>>m>>q;
+
+    for(int i=0;i<n;i++){
+        int tmp1,tmp2;
+        cin>>tmp1>>tmp2;
+        a[tmp1][tmp2]=1;
+    }
+
+
+
 
 }
